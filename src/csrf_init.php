805fcb8938e808ea2cca24891f177190ca8d50fb
Yasuo Ohgaki <yohgaki@php.net>
2018-06-02 15:12:06 +0900

    Fix csrf_init

--- src/csrf_init.php
+++ src/csrf_init.php
@@ -16,8 +16,9 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 $_SESSION['CSRF_SECRET'] = $_SESSION['CSRF_SECRET'] ?? random_bytes(32);
 $csrf_token = $_POST['csrftk'] ?? ($_GET['csrftk'] ?? '');
 
-$keys = json_decode(base64_decode($csrf_token), true);
-if (empty($keys['expire']) || $keys['expire'] < time() - 900) {
+$valid = csrf_validate_token($_SESSION['CSRF_SECRET'], $csrf_token);
+$keys  = json_decode(base64_decode($csrf_token), true);
+if ($valid !== true || empty($keys['expire']) || $keys['expire'] < time() - 900) {
     $csrf_token_new = csrf_generate_token($_SESSION['CSRF_SECRET']);
 }
 output_add_rewrite_var('csrftk', $csrf_token_new ?? $csrf_token);
@@ -26,7 +27,6 @@ if (!$csrf_token) {
     throw new RuntimeException('CSRF Token validation error: No token');
 }
 
-$ret = csrf_validate_token($_SESSION['CSRF_SECRET'], $csrf_token);
-if ($ret !== true) {
+if ($valid !== true) {
     throw new RuntimeException('CSRF Token validation error: '. $ret);
 }

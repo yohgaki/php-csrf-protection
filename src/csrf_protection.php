3b7b5f531907ad9cf62c966d5d407a122562963b
Yasuo Ohgaki <yohgaki@php.net>
2018-06-02 21:17:19 +0900

    Use HEX

--- src/csrf_protection.php
+++ src/csrf_protection.php
@@ -20,9 +20,9 @@ function csrf_generate_token($secret, $expire = 300)
     assert(is_int($expire) && $expire >= 60);
 
     $uri = parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
-    $salt = base64_encode(random_bytes(33));
+    $salt = bin2hex(random_bytes(33));
     $expire += time();
-    $key = base64_encode(hash_hkdf('sha256', $secret, 0, $uri."\0".$expire, $salt));
+    $key = bin2hex(hash_hkdf('sha256', $secret, 0, $uri."\0".$expire, $salt));
     $token = join("-", [$salt, $key, $expire]);
     assert(strlen($token) > 32);
     return $token;
@@ -55,7 +55,7 @@ function csrf_validate_token($secret, $token)
     if ($expire < time()) {
         return 'Expired';
     }
-    $key2 = base64_encode(hash_hkdf('sha256', $secret, 0, $uri."\0".$expire, $salt));
+    $key2 = bin2hex(hash_hkdf('sha256', $secret, 0, $uri."\0".$expire, $salt));
     if (hash_equals($key, $key2) === false) {
         return 'Key mismatch';
     }
